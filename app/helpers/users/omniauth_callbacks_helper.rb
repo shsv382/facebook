ac283c2f9363a667119a28125e2988e38b1cc48c
shsv382 <shsv382@gmail.com>
2018-06-02 18:48:55 +0300

    Added vk-omniauth

--- app/helpers/users/omniauth_callbacks_helper.rb
+++ app/helpers/users/omniauth_callbacks_helper.rb
@@ -1,2 +1,3 @@
 module Users::OmniauthCallbacksHelper
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,12 @@ class User < ApplicationRecord
     end
 
     def self.find_for_vkontakte_oauth access_token
-    	if user = User.where(:name => access_token.extra.raw_info.name).first
+    	if user = User.where(:name => access_token.info.name).first
     		user
     	else
-    		User.create!(#provider: access_token.provider,
-    			name:  access_token.extra.raw_info.name,
-    			email: access_token.extra.raw_info.name + '@vk.com',
+    		user = User.create!(#provider: access_token.provider,
+    			name:  access_token.info.name,
+    			email: access_token.info.name.split[0] + rand(265536).to_s + '@vk.com',
     			password: Devise.friendly_token[0,20])
     	end
     end
